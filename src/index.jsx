6fcb088f5ff2ef0848c721b847a9873c695275d0
Diego Muracciole <diegomuracciole@gmail.com>
2024-02-18 21:48:17 +0100

    fix: build

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
-import App from './app.js';
+import App from './app';
 
 const container = document.getElementById('root');
 
